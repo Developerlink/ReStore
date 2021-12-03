18fb0326ea88128e6b97c741d559deff6c803b6d
Trung Kien Huynh <trungkien.huynh@gmail.com>
2021-12-03 14:19:07 +0100

    Add project files.

---
+++ ReStoreDataAccessLibrary/StoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReStoreModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReStoreDataAccessLibrary
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}

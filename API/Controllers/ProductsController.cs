5887f3bc047a25e3fcf685579f5d2f07f8394b48
Trung Kien Huynh <trungkien.huynh@gmail.com>
2021-12-06 12:44:42 +0100

    Added ProductsController and data-seeding

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReStoreDataAccessLibrary;
+using ReStoreModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+
+        public ProductsController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _context.Products.FindAsync(id) ;
+
+            return Ok(product);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public async Task<ActionResult<Product>> Post([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _context.AddAsync(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

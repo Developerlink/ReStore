c7ba9c691bd970e1ce6f806c38b438a6472a6cf1
Trung Kien Huynh <trungkien.huynh@gmail.com>
2021-12-09 15:08:33 +0100

    Added routing and work on details page

--- client/src/features/catalog/Header.tsx
+++ client/src/features/catalog/Header.tsx
@@ -1,8 +1,19 @@
-import { AppBar, List, ListItem, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import Switch from "@mui/material/Switch";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
+import { ShoppingCart } from "@mui/icons-material";
 
 interface Props {
   handleThemeChange: () => void;
@@ -20,26 +31,65 @@ const rightLinks = [
   { title: "register", to: "/register" },
 ];
 
+const navStyles = {
+  typography: "h6",
+  "&:hover": {
+    color: "grey.500",
+  },
+};
+
 const Header = ({ handleThemeChange, isDarkMode }: Props) => {
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
-      <Toolbar>
-        <Typography variant="h6">ReStore</Typography>
-        <Switch
-          checked={isDarkMode}
-          onChange={handleThemeChange}
-          icon={<DarkModeIcon />}
-          checkedIcon={<LightModeIcon />}
-          color="default"
-          inputProps={{ "aria-label": "Dark mode switch" }}
-        />
-        <List>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box display='flex' alignItems='center'>
+          <Typography variant="h6" sx={navStyles}>
+            <NavLink className={styles.navlink} to="/">
+              RE-STORE
+            </NavLink>
+          </Typography>
+          <Switch
+            checked={isDarkMode}
+            onChange={handleThemeChange}
+            icon={<DarkModeIcon />}
+            checkedIcon={<LightModeIcon />}
+            color="default"
+            inputProps={{ "aria-label": "Dark mode switch" }}
+          />
+        </Box>
+
+        <List sx={{ display: "flex" }}>
           {midLinks.map(({ title, to }) => (
-            <ListItem key={to} sx={{ color: "inherit", typography: "h6" }}>
-              <NavLink to={to}>{title.toUpperCase()}</NavLink>
+            <ListItem key={to} sx={navStyles}>
+              <NavLink className={styles.navlink} to={to}>
+                {title.toUpperCase()}
+              </NavLink>
             </ListItem>
           ))}
         </List>
+
+        <Box display='flex' alignItems='center'>
+          <IconButton size="large" sx={{ color: "inherit" }}>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, to }) => (
+              <ListItem key={to} sx={navStyles}>
+                <NavLink className={styles.navlink} to={to}>
+                  {title.toUpperCase()}
+                </NavLink>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -8,7 +8,9 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 import { Product } from "../../models/product";
+import styles from "./ProductCard.module.css";
 
 interface Props {
   product: Product;
@@ -18,22 +20,26 @@ const ProductCard = ({ product }: Props) => {
   return (
     <Card sx={{ maxWidth: 300 }}>
       <CardHeader
-        avatar={<Avatar sx={{ bgcolor: 'secondary.main' }}>{product.name.charAt(0).toUpperCase()}</Avatar>}
+        avatar={
+          <Avatar sx={{ bgcolor: "secondary.main" }}>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
         title={product.name}
         titleTypographyProps={{
-            sx: {fontWeight: 'bold', color: 'primary.main'}
+          sx: { fontWeight: "bold", color: "primary.main" },
         }}
       />
       <CardMedia
-        height='100'
+        height="100"
         component="img"
-        sx={{ objectFit: 'contain'}}
+        sx={{ objectFit: "contain" }}
         image={product.pictureUrl}
         title={product.name}
       />
       <CardContent>
         <Typography gutterBottom color="secondary" variant="h5">
-          Kr {(product.price/100).toFixed(2)} 
+          Kr {(product.price / 100).toFixed(2)}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {product.brand} / {product.type}
@@ -41,7 +47,9 @@ const ProductCard = ({ product }: Props) => {
       </CardContent>
       <CardActions>
         <Button size="small">Add to cart</Button>
-        <Button size="small">View</Button>
+        <Button size="small">
+          <Link className={styles.link} to={product.id.toString()}>View</Link>
+        </Button>
       </CardActions>
     </Card>
   );

f4daa869aae5a6e32600b418778982cb39f26f7e
Trung Kien Huynh <trungkien.huynh@gmail.com>
2022-01-20 14:49:38 +0100

    Published React project now works inside the API app

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
         public IActionResult Index()
         {
             return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
-                "wwroot", "index.html"), "text/HTML");
+                "wwwroot", "index.html"), "text/HTML");
         }
     }
 }
